0375a83e82d1fed15e028cc7f97aefa4c31e83d0
IamSila <silamulingi@gmail.com>
2025-12-06 19:48:49 +0300

    register page updates

--- studentPortal/views.py
+++ studentPortal/views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-
+from .forms import CreateUserForm
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
     context = {"form": form}
     return render(request, "register.html", context)
 
