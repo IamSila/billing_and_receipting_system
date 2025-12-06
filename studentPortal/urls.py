0db6959e903e2ec872ba2327e4df73364ec7ede3
IamSila <silamulingi@gmail.com>
2025-12-06 19:04:12 +0300

    student account registration

--- studentPortal/urls.py
+++ studentPortal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = "studentPortal"
 urlpatterns = [
-# remember to change this path and the view
-        path("home/", views.home, name='home'),
+    # remember to change this path and the view
+    path("home/", views.home, name="home"),
+    path("register/", views.register, name="register"),
 ]
--- studentPortal/views.py
+++ studentPortal/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
 
 
+def register(request):
+    form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
 def home(request):
-  return render(request, 'portal.html')
+    return render(request, "portal.html")
